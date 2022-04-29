a6d1d1ca3b289c2381399d2821dcd0c7a790a581
Alexandr Sanatar <allexvik@gmail.com>
2022-04-29 12:32:43 +0300

    Updates

---
+++ static/js/6.0c0166a1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[6],{6:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("p",{children:"Not Found 404!"})}}}]);
+//# sourceMappingURL=6.0c0166a1.chunk.js.map
